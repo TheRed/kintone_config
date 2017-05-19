b31e0c36c1b25e8476f0db85d28dd352b5c34352
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-05-19 16:13:01 +0900

    課題4: 関数に分ける(コンソール出力処理も関数内)

--- kintone_config.js
+++ kintone_config.js
@@ -1,34 +1,38 @@
 jQuery(function($) {
   'use strict';
 
+  function getFieldProps(body) {
+    kintone.api(kintone.api.url('/k/v1/app/form/fields', true), 'GET', body, function(resp) {
+      var properties = resp.properties;
+      var str = 'フィールドコード,フィールド名,フィールドの種類\n';
+      for (var key in properties) {
+        var prop = properties[key];
+        str += key + ',' + prop.label + ',' + prop.type + '\n';
+      }
+      console.log(str);
+    }, function(error) {
+      console.log(error);
+    });
+  }
+
   kintone.events.on('app.record.index.show', function(event) {
     // ボタン増殖を防ぐ
     if ($('#get_config_button')[0]) {
       return;
     }
 
-    var appId = event.appId;
-    var body = {
-      app: appId,
-      lang: 'ja',
-    }
 
     // フォームのフィールド一覧を取得ボタン
     var $getConfigButon = $('<button>', {
       id: 'get_config_button',
       text: 'フォーム設定の取得',
     }).click(function() {
-      kintone.api(kintone.api.url('/k/v1/app/form/fields', true), 'GET', body, function(resp) {
-        var properties = resp.properties;
-        var str = 'フィールドコード,フィールド名,フィールドの種類\n';
-        for (var key in properties) {
-          var prop = properties[key];
-          str += key + ',' + prop.label + ',' + prop.type + '\n';
-        }
-        console.log(str);
-      }, function(error) {
-        console.log(error);
-      });
+      var appId = event.appId;
+      var body = {
+        app: appId,
+        lang: 'ja',
+      }
+      getFieldProps(body);
     }); // click();
 
     // メニューの空白部分にボタンを設定
